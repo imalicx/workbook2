8b13545d9ebfa7b22d003ee7d9e97769c77b5573
imalicx <xenrecords@yahoo.com>
2023-04-25 10:09:36 -0400

    wip

---
+++ LectureDemos/exercise3.js
@@ -0,0 +1,10 @@
+var firstName;
+firstname = "Malic"
+
+var city;
+city = "Northern Miami,"
+
+var state;
+state = "Florida"
+
+console.log("Your name is " + firstName "I am from" + city + state);
---
+++ LectureDemos/firstcode.js
@@ -0,0 +1,19 @@
+// The following is a script
+// that does a few things.
+
+/*
+The following is a script
+that does a few things.
+
+*/
+var firstName;
+
+firstName = "Malic";
+
+var greeting;
+
+greeting = "Your name is " + firstName; //This line will assign the result of an expression to the greeting variable.
+
+console.log(greeting);
+
+console.log("Your name is " + firstName);
